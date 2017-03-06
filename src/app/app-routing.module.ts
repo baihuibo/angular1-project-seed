7b09f25e2498ae0c3d4e74b0bc856bf237650d90
baihuibo <baihuibo@hotmail.com>
2017-03-06 09:52:26 +0800

    --

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,27 @@ import {RouterModule, IRouter} from "router";
 import {PageAComponent} from "./pagea/pagea.component";
 import {PageAGuard} from "./pagea/pagea.guard";
 
-const configs: IRouter[] = [{
-    url: '/pagea',
-    name: 'pagea',
-    component: PageAComponent,
-    canActivate: [PageAGuard]
-}, {
-    url: '/pageb',
-    name: 'pageb',
-    component: 'app-async-page',
-    resolve: {
-        loadChildren: "./pageb/async-page/async-page.module.ts#AsyncPageModule"
+const configs: IRouter[] = [
+    {
+        url: '/pagea',
+        name: 'pagea',
+        component: PageAComponent,
+        canActivate: [PageAGuard]
+    }, {
+        url: '/pageb',
+        name: 'pageb',
+        component: 'app-async-page',
+        resolve: {
+            loadChildren: "./pageb/async-page/async-page.module.ts#AsyncPageModule"
+        }
+    }, {
+        url: '',
+        redirectTo: '/pagea',
+    }, {
+        url: '**',
+        redirectTo: '/pagea'
     }
-}, {
-    url: '',
-    redirectTo: '/pagea',
-}, {
-    url: '**',
-    redirectTo: '/pagea'
-}];
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(configs)]
